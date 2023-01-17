9798b7d939ceaf722814fa91e94a271ec3890d95
marksabelita <marksabelita@gmail.com>
2023-01-18 03:40:48 +0800

    WIP Event

---
+++ src/interface/models/event.interface.ts
@@ -0,0 +1,12 @@
+export interface IEventInterfaceModel {
+  mainEventImage: string
+  frequency?: string
+  accomodation_cout?: number
+  typeOfEvent?: number
+  categoryOfEvent?: string
+  timeZone?: string
+  summary?: string
+  fullDescription?: string
+}
+
+export interface IEventSchduleModel {}
