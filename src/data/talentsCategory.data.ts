103dbf0cd7221be6f338351318b223e07e79459f
marksabelita <marksabelita@gmail.com>
2023-01-18 02:36:38 +0800

    Added static categories

---
+++ src/data/talentsCategory.data.ts
@@ -0,0 +1,66 @@
+export const LOKAL_TALENT_CATEGORIES_DATA = [
+  {
+    id: 'LBRWRK01_CNSTRCTNRCHTCHTR',
+    name: 'CONSTURCTION AND ARCHITECHTURE',
+    subCategory: [
+      {
+        id: 'LBRWRK01_CNWRKR',
+        name: 'Construction worker',
+      },
+      {
+        id: 'LBRWRK01_FRMN',
+        name: 'Foreman',
+      },
+      {
+        id: 'LBRWRK01_PNTR',
+        name: 'Painter',
+      },
+      {
+        id: 'LBRWRK01_WLDR',
+        name: 'Welder',
+      },
+    ],
+  },
+  {
+    id: 'LBRWRK02_UTMBLVHCL',
+    name: 'AUTO MOBILE & VEHICLE',
+    subCategory: [
+      {
+        id: 'LBRWRK02_TMCHNC',
+        name: 'Auto Mechanic',
+      },
+      {
+        id: 'LBRWRK02_RCNMCHNC',
+        name: 'Aircon Mechanic',
+      },
+      {
+        id: 'LBRWRK02_MTRCYCLMCHN',
+        name: 'Motorcycle Mechanic',
+      },
+    ],
+  },
+  {
+    id: 'LBRWRK03_DLVRY',
+    name: 'Delivery',
+    subCategory: [
+      {
+        id: 'LBRWRK03_MTRCYCL',
+        name: 'Motorcycle Delivery',
+      },
+      {
+        id: 'LBRWRK03_VHCL',
+        name: 'Vehicle Delivery',
+      },
+    ],
+  },
+  {
+    id: 'LBRWRK04_HLPR',
+    name: 'Helper',
+    subCategory: [
+      {
+        id: 'LBRWRK04_',
+        name: 'General',
+      },
+    ],
+  },
+]
